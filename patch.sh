d2f6c84f54ea026e86a1f521b0bec4042b21fe58
Wim Roose <systeembeheer@vooruit.be>
2014-09-09 15:02:11 +0200

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -8,8 +8,12 @@ git clone https://github.com/vooruit/screenly_patch.git
 # compileer grabScreenshot
 cd screenly_patch/tools
 gcc -O3 grabScreenshot.c -o grabScreenshot -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -Wno-psabi -I/opt/vc/include/ -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -L/opt/vc/lib/ -lGLESv2 -lEGL
+cd ..
 
 sudo echo "/1 * * * root /home/pi/tools/grabScreenshot.sh" > /etc/crontab
 sudo /etc/init.d/cron restart
+
+cp index.haml ../screenly/views/
+
 
 
