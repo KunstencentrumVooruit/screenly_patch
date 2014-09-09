cb1467bfd30dded24917478b2f4bc1b5b2ba70ec
Wim Roose <systeembeheer@vooruit.be>
2014-09-09 15:54:56 +0200

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -9,6 +9,7 @@ git clone https://github.com/vooruit/screenly_patch.git
 cd screenly_patch/tools
 gcc -O3 grabScreenshot.c -o grabScreenshot -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -Wno-psabi -I/opt/vc/include/ -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -L/opt/vc/lib/ -lGLESv2 -lEGL
 cd ..
+mv tools /home/pi/
 
 # crontab lijn toevoegen met sed. Via echo permission denied
 sudo sed -i '$ a\*/1 * * * root /home/pi/tools/grabScreenshot.sh' /etc/crontab
