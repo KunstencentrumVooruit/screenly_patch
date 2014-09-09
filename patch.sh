90068282eef892313e72381b16385d23e8f2a8e5
Wim Roose <systeembeheer@vooruit.be>
2014-09-09 16:12:12 +0200

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -7,7 +7,8 @@ git clone https://github.com/vooruit/screenly_patch.git
 
 # compileer grabScreenshot
 cd screenly_patch/tools
-gcc -O3 grabScreenshot.c -o grabScreenshot -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -Wno-psabi -I/opt/vc/include/ -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -L/opt/vc/lib/ -lGLESv2 -lEGL
+#gcc -O3 grabScreenshot.c -o grabScreenshot -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -Wno-psabi -I/opt/vc/include/ -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -L/opt/vc/lib/ -lGLESv2 -lEGL
+wget http://192.168.2.22/grabScreenshot
 cd ..
 mv tools /home/pi/
 
